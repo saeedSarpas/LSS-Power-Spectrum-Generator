a8cdfd8ac654dcb797cddf760829715130e650b8
Saeed Sarpas <saeed@astro.uni-bonn.de>
2015-11-04 19:39:17 +0100

    Added triangular shaped cloud griding Extracted boundry checker from threeToOne function

--- headers/griding.h
+++ headers/griding.h
@@ -1,26 +1,22 @@
-int threeToOne(int i, int j, int k) {
-    if (i > numGrid - 1) {
-        i = i - numGrid;
-    } else if (i < 0) {
-        i = i + numGrid;
+int gridBoundryChecker (int pos) {
+    if (pos > numGrid - 1) {
+        return pos - numGrid;
+    } else if (pos < 0) {
+        return pos + numGrid;
+    } else {
+        return pos;
     }
-
-    if (j > numGrid - 1) {
-        j = j - numGrid;
-    } else if (j < 0) {
-        j = j + numGrid;
-    }
-
-    if (k > numGrid - 1) {
-        k = k - numGrid;
-    } else if (k < 0) {
-        k = k + numGrid;
-    }
-
-    return(k * numGrid * numGrid + j * numGrid + i);
 }
 
-void NGP (struct particle_data P, float * mass_grid) {
+int threeToOne(int i, int j, int k) {
+    i = gridBoundryChecker(i);
+    j = gridBoundryChecker(j);
+    k = gridBoundryChecker(k);
+
+    return k * numGrid * numGrid + j * numGrid + i;
+}
+
+void NGP (struct particle_data P, float * grid_mass) {
     int i, j, k;
 
     i = P.Pos[0] / gridSize;
@@ -29,7 +25,7 @@ void NGP (struct particle_data P, float * mass_grid) {
 
     int index = threeToOne(i, j, k);
 
-    mass_grid[index] += P.Mass;
+    grid_mass[index] += P.Mass;
 }
 
 void cicWeight (int index, int * delta, float overlap, float * weight) {
@@ -42,47 +38,83 @@ void cicWeight (int index, int * delta, float overlap, float * weight) {
     }
 }
 
-void CIC (struct particle_data P, float * mass_grid) {
+void CIC (struct particle_data P, float * grid_mass) {
     int i, j, k;
     int di, dj, dk;
 
     float i_weight, j_weight, k_weight;
-    float overlap_temp;
+    float overlap;
 
     i = P.Pos[0] / gridSize;
-    overlap_temp = fmodf(P.Pos[0], gridSize);
-    cicWeight(i, &di, overlap_temp, &i_weight);
+    overlap = fmodf(P.Pos[0], gridSize);
+    cicWeight(i, &di, overlap, &i_weight);
 
     j = P.Pos[1] / gridSize;
-    overlap_temp = fmodf(P.Pos[1], gridSize);
-    cicWeight(j, &dj, overlap_temp, &j_weight);
+    overlap = fmodf(P.Pos[1], gridSize);
+    cicWeight(j, &dj, overlap, &j_weight);
 
     k = P.Pos[2] / gridSize;
-    overlap_temp = fmodf(P.Pos[2], gridSize);
-    cicWeight(k, &dk, overlap_temp, &k_weight);
+    overlap = fmodf(P.Pos[2], gridSize);
+    cicWeight(k, &dk, overlap, &k_weight);
 
-    int index_temp;
-    index_temp = threeToOne(i, j, k);
-    mass_grid[index_temp] += i_weight * j_weight * k_weight * P.Mass;
+    int index;
+    index = threeToOne(i, j, k);
+    grid_mass[index] += i_weight * j_weight * k_weight * P.Mass;
 
-    index_temp = threeToOne(i+di, j, k);
-    mass_grid[index_temp] += (1 - i_weight) * j_weight * k_weight * P.Mass;
+    index = threeToOne(i+di, j, k);
+    grid_mass[index] += (1 - i_weight) * j_weight * k_weight * P.Mass;
 
-    index_temp = threeToOne(i, j+dj, k);
-    mass_grid[index_temp] += i_weight * (1 - j_weight) * k_weight * P.Mass;
+    index = threeToOne(i, j+dj, k);
+    grid_mass[index] += i_weight * (1 - j_weight) * k_weight * P.Mass;
 
-    index_temp = threeToOne(i, j, k+dk);
-    mass_grid[index_temp] += i_weight * j_weight * (1 - k_weight) * P.Mass;
+    index = threeToOne(i, j, k+dk);
+    grid_mass[index] += i_weight * j_weight * (1 - k_weight) * P.Mass;
 
-    index_temp = threeToOne(i+di, j+dj, k);
-    mass_grid[index_temp] += (1 - i_weight) * (1 - j_weight) * k_weight * P.Mass;
+    index = threeToOne(i+di, j+dj, k);
+    grid_mass[index] += (1 - i_weight) * (1 - j_weight) * k_weight * P.Mass;
 
-    index_temp = threeToOne(i, j+dj, k+dk);
-    mass_grid[index_temp] += i_weight * (1 - j_weight) * (1 - k_weight) * P.Mass;
+    index = threeToOne(i, j+dj, k+dk);
+    grid_mass[index] += i_weight * (1 - j_weight) * (1 - k_weight) * P.Mass;
 
-    index_temp = threeToOne(i+di, j, k+dk);
-    mass_grid[index_temp] += (1 - i_weight) * j_weight * (1 - k_weight) * P.Mass;
+    index = threeToOne(i+di, j, k+dk);
+    grid_mass[index] += (1 - i_weight) * j_weight * (1 - k_weight) * P.Mass;
 
-    index_temp = threeToOne(i+di, j+dj, k+dk);
-    mass_grid[index_temp] += (1 - i_weight) * (1 - j_weight) * (1 - k_weight) * P.Mass;
+    index = threeToOne(i+di, j+dj, k+dk);
+    grid_mass[index] += (1 - i_weight) * (1 - j_weight) * (1 - k_weight) * P.Mass;
+}
+
+int moveAlongGridAxis(int init_pos, int step) {
+    int new_pos = init_pos + step;
+    return gridBoundryChecker(new_pos);
+}
+
+void TSC (struct particle_data P, float * grid_mass) {
+    int pos[3];
+    float overlap;
+    float weight[3][3];
+
+    int i;
+    for (i = 0; i < 3; i++) {
+        pos[i] = P.Pos[i] / gridSize;
+        overlap = fmodf(P.Pos[i], gridSize);
+
+        float overlap_ratio = overlap/gridSize;
+        weight[i][0] = 0.5  * pow((1 - overlap_ratio), 2);
+        weight[i][1] = 0.75 - pow((overlap_ratio - 0.5), 2);
+        weight[i][2] = 0.5  * pow(overlap_ratio, 2);
+    }
+
+    int l, m, n, index, neighbor_pos[3];
+    for (l = 0; l < 3; l++) {
+        for (m = 0; m < 3; m++) {
+            for (n = 0; n < 3; n++) {
+                neighbor_pos[0] = moveAlongGridAxis(pos[0], l - 1);
+                neighbor_pos[1] = moveAlongGridAxis(pos[1], m - 1);
+                neighbor_pos[2] = moveAlongGridAxis(pos[2], n - 1);
+
+                index = threeToOne(neighbor_pos[0], neighbor_pos[1], neighbor_pos[2]);
+                grid_mass[index] += weight[0][l] * weight[1][m] * weight[2][n] * P.Mass;
+            }
+        }
+    }
 }
--- main.c
+++ main.c
@@ -8,28 +8,34 @@
 int main() {
     loadInputFromFile(); // From load_input header file
 
-    float * mass_grid_ptr;
-    mass_grid_ptr = calloc(pow(numGrid, 3), sizeof(float));
+    float * grid_mass_ptr;
+    grid_mass_ptr = calloc(pow(numGrid, 3), sizeof(float));
 
     char algorithm;
-    fprintf(stdout, "Choose griding algorithm, [n for NGP (defualt), c for CIC]: ");
+    fprintf(stdout, "Choose griding algorithm, [n for NGP (defualt), c for CIC, t for TSC]: ");
     fscanf(stdin, "%c", &algorithm);
 
     int n;
     if (algorithm == 'c') {
       fprintf(stdout, "Griding using cloud in cell (CIC) algorithm... ");
       for (n = 0; n < numPart; n++) {
-        CIC(P[n], mass_grid_ptr);
+        CIC(P[n], grid_mass_ptr);
+      }
+      fprintf(stdout, "[done]\n");
+    } else if (algorithm == 't') {
+      fprintf(stdout, "Griding using triangular shaped cloud (TSC) algorithm... ");
+      for (n = 0; n < numPart; n++) {
+        TSC(P[n], grid_mass_ptr);
       }
       fprintf(stdout, "[done]\n");
     } else {
       fprintf(stdout, "Griding using nearest grid points (NGP) algorithm... ");
       for (n = 0; n < numPart; n++) {
-        NGP(P[n], mass_grid_ptr);
+        NGP(P[n], grid_mass_ptr);
       }
       fprintf(stdout, "[done]\n");
     }
 
-    free(mass_grid_ptr);
+    free(grid_mass_ptr);
     return 0;
 }
