3e217a3b3ce6ea43f518162d69ce3cdd39493da2
Saeed Sarpas <saeed@astro.uni-bonn.de>
2016-01-31 22:32:10 +0100

    Refactored open_file to report right error

--- global_functions/open_file.c
+++ global_functions/open_file.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <string.h>
 
 void open_file(FILE **file, char *path, char *mode) {
 	if (!(*file = fopen(path, mode))) {
-		printf("[Cannot open file %s]\n", path);
+		printf("[fopen error: %s (%s)]\n", strerror(errno), path);
 		exit(0);
 	}
 }
