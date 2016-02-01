00e2bc1130d11852eefaa71ddeaa1dccc01ca6ac
Saeed Sarpas <saeed@astro.uni-bonn.de>
2016-02-01 16:48:56 +0100

    [bug fixed] in read_modes_struct due to name confliction

--- global_functions/io/read_modes_struct_from.c
+++ global_functions/io/read_modes_struct_from.c
@@ -3,13 +3,10 @@
 #include <math.h>
 
 #include "./../../global_structs/modes_struct.h"
-#include "./../../global_structs/config_struct.h"
 
-void read_modes_struct_from(FILE *file, char *path, modes *modes,
-		size_t size, config *conf) {
-	int tot_num_of_grids = pow(conf->num_of_grids_in_each_axis, 3);
-	if ( fread(modes, sizeof(modes), tot_num_of_grids, file)
-			!= tot_num_of_grids) {
+void read_modes_struct_from(FILE *file, char *path, modes *indexed_modes,
+		size_t size) {
+	if (fread(indexed_modes, sizeof(modes), size, file) != size) {
 		printf("[Cannot read from file %s]", path);
 		exit(0);
 	};
--- global_functions/io/read_modes_struct_from.h
+++ global_functions/io/read_modes_struct_from.h
@@ -1,4 +1,3 @@
 #include "./../../global_structs/modes_struct.h"
-#include "./../../global_structs/config_struct.h"
 
-void read_modes_struct_from(FILE *, char *, modes *, size_t, config *);
+void read_modes_struct_from(FILE *, char *, modes *, size_t);
