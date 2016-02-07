04044536e8539d4dfea1ad4fac95e8c90ff3e1b8
Saeed Sarpas <saeed@astro.uni-bonn.de>
2016-02-08 00:03:22 +0100

    Refactored reordering test

--- 3_fftw/src/include/reordering_fourier_input_test.c
+++ 3_fftw/src/include/reordering_fourier_input_test.c
@@ -10,67 +10,74 @@
 #include "./reordering_fourier_input.h"
 
 Describe(reordering_fourier_input);
-BeforeEach(reordering_fourier_input) {}
+
+#define NUM_OF_GRIDS 4
+#define TOT_NUM_OF_GRIDS 64
+#define REAL_PART 1.2
+#define IMAG_PART 3.4
+
+static config_struct conf;
+static fftw_complex delta_complex[TOT_NUM_OF_GRIDS];
+static void fill_delta_complex(fftw_complex*);
+
+BeforeEach(reordering_fourier_input) {
+	conf.num_of_grids_in_each_axis = NUM_OF_GRIDS;
+	fill_delta_complex(delta_complex);
+}
+
 AfterEach(reordering_fourier_input) {}
 
-Ensure(reordering_fourier_input, mirrors_specific_elements) {
-	config_struct conf;
-	conf.num_of_grids_in_each_axis = 2;
-
-	fftw_complex delta_complex[8];
-	int i;
-	for (i = 0; i < 8; i ++)
-		delta_complex[i] = 1.2 + 3.4 * I;
-
+Ensure(reordering_fourier_input, mirrors_right_elements) {
 	reordering_fourier_input(delta_complex, &conf);
 
-	int j, k;
-	for (i = 0; i < 2; i++) {
-		for (j = 0; j < 2; j++) {
-			for (k = 0; k < 2; k++) {
+	int i, j, k;
+	for (i = 0; i < conf.num_of_grids_in_each_axis; i++) {
+		for (j = 0; j < conf.num_of_grids_in_each_axis; j++) {
+			for (k = 0; k < conf.num_of_grids_in_each_axis; k++) {
 				int s = pow(-1, (i + j + k));
 				int index = three_to_one(i, j, k, &conf);
-				assert_that(creal(delta_complex[index]), is_equal_to(s * 1.2));
-				assert_that(cimag(delta_complex[index]), is_equal_to(s * 3.4));
+				assert_that(creal(delta_complex[index]),
+							is_equal_to(s * REAL_PART));
+				assert_that(cimag(delta_complex[index]),
+							is_equal_to(s * IMAG_PART));
 			}
 		}
 	}
 }
 
-Ensure(reordering_fourier_input, puts_dc_component_at_the_center_of_delta_dourier) {
-	config_struct conf;
-	conf.num_of_grids_in_each_axis = 4;
+Ensure(reordering_fourier_input,
+	   puts_dc_component_at_the_center_of_delta_fourier) {
+	fftw_complex delta_fourier[TOT_NUM_OF_GRIDS];
+	int rank[3] = {NUM_OF_GRIDS, NUM_OF_GRIDS, NUM_OF_GRIDS};
+	fftw_plan plan = fftw_plan_dft(3, rank, delta_complex, delta_fourier,
+								   FFTW_FORWARD, FFTW_MEASURE);
 
-	int rank[3] = {4, 4, 4};
-	fftw_complex delta_complex[64];
-	fftw_complex delta_fourier[64];
-
-	fftw_plan p;
-	p = fftw_plan_dft(3, rank, delta_complex, delta_fourier, FFTW_FORWARD,
-						FFTW_MEASURE);
-
-	int i;
-	for (i = 0; i < 64; i ++)
-		delta_complex[i] = 1.2 + 3.4 * I;
-
-	fftw_execute(p);
-	fftw_complex dc = delta_fourier[0];
+	fill_delta_complex(delta_complex);
 
 	reordering_fourier_input(delta_complex, &conf);
+	fftw_execute(plan);
 
-	fftw_execute(p);
+	fftw_complex dc = TOT_NUM_OF_GRIDS * (REAL_PART + IMAG_PART * I);
 
-
-	int index = three_to_one(2, 2, 2, &conf);
+	int index = three_to_one(NUM_OF_GRIDS/2, NUM_OF_GRIDS/2, NUM_OF_GRIDS/2,
+							 &conf);
 	assert_that(creal(delta_fourier[index]), is_equal_to(creal(dc)));
 	assert_that(cimag(delta_fourier[index]), is_equal_to(cimag(dc)));
 
-	fftw_destroy_plan(p);
+	fftw_destroy_plan(plan);
 }
 
 TestSuite *reordering_fourier_input_tests() {
 	TestSuite *suite = create_test_suite();
-	add_test_with_context(suite, reordering_fourier_input, mirrors_specific_elements);
-	add_test_with_context(suite, reordering_fourier_input, puts_dc_component_at_the_center_of_delta_dourier);
+	add_test_with_context(suite, reordering_fourier_input,
+						  mirrors_right_elements);
+	add_test_with_context(suite, reordering_fourier_input,
+						  puts_dc_component_at_the_center_of_delta_fourier);
 	return suite;
+}
+
+void fill_delta_complex(fftw_complex *d) {
+	int i;
+	for (i = 0; i < TOT_NUM_OF_GRIDS; i++)
+		d[i] = REAL_PART + IMAG_PART * I;
 }
