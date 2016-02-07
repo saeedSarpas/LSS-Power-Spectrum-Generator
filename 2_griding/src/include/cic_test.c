47a225df676eedfe65f23a0bd23085b6e41e782d
Saeed Sarpas <saeed@astro.uni-bonn.de>
2016-02-08 00:28:54 +0100

    Refactored griding tests

--- 2_griding/src/include/cic_test.c
+++ 2_griding/src/include/cic_test.c
@@ -12,104 +12,69 @@
 #include "./cic.h"
 
 Describe(cic);
-BeforeEach(cic) {}
-AfterEach(cic) {}
 
-Ensure(cic, returns_right_value_for_a_trivial_case) {
-	config_struct conf;
+#define NUM_OF_GRIDS 10
+#define NUM_OF_PARTS 10
+#define BOX_LENGTH 10
+
+static config_struct conf;
+static int tot_num_of_grids;
+static input_info_struct info;
+static particle_data_struct *P;
+static double *grid_mass;
+static void fill_P (double, double, double, double);
+
+BeforeEach(cic) {
 	conf.num_of_grids_in_each_axis = 10;
-	int tot_num_of_grids = pow(conf.num_of_grids_in_each_axis, 3);
+	tot_num_of_grids = pow(conf.num_of_grids_in_each_axis, 3);
 
-	input_info_struct info;
-	info.num_of_parts = 10;
-	info.box_length = 10;
+	info.num_of_parts = NUM_OF_PARTS;
+	info.box_length = BOX_LENGTH;
 
-	particle_data_struct *P;
 	allocate_particle_data_struct(&P, info.num_of_parts);
 
-	int i;
-	for (i = 0; i < info.num_of_parts; i++) {
-		P[i].Pos[0] = 4.5;
-		P[i].Pos[1] = 4.5;
-		P[i].Pos[2] = 4.5;
-		P[i].Mass = 1;
-	}
-
-	double *grid_mass;
 	allocate_double_array(&grid_mass, tot_num_of_grids);
+}
 
+AfterEach(cic) {
+	free(P);
+	free(grid_mass);
+}
+
+Ensure(cic, returns_right_value_for_a_trivial_case) {
+	fill_P(4.5, 4.5, 4.5, 1);
 	cic(P, grid_mass, &info, &conf);
 
+	int i, index = three_to_one(4, 4, 4, &conf);
 	for (i = 0; i < tot_num_of_grids; i++) {
-		int index = three_to_one(4, 4, 4, &conf);
 		if (i != index)
 			assert_that(grid_mass[i], is_equal_to(0));
 		else
-			assert_that(grid_mass[i], is_equal_to(10));
+			assert_that(grid_mass[i], is_equal_to(1 * info.num_of_parts));
 	}
 }
 
 Ensure(cic, calculate_the_share_of_each_cell_right) {
-	config_struct conf;
-	conf.num_of_grids_in_each_axis = 10;
-	int tot_num_of_grids = pow(conf.num_of_grids_in_each_axis, 3);
-
-	input_info_struct info;
-	info.num_of_parts = 10;
-	info.box_length = 10;
-
-	particle_data_struct *P;
-	allocate_particle_data_struct(&P, info.num_of_parts);
-
-	int i;
-	for (i = 0; i < info.num_of_parts; i++) {
-		P[i].Pos[0] = 4;
-		P[i].Pos[1] = 4;
-		P[i].Pos[2] = 4;
-		P[i].Mass = 1;
-	}
-
-	double *grid_mass;
-	allocate_double_array(&grid_mass, tot_num_of_grids);
-
+	fill_P(4.0, 4.0, 4.0, 1);
 	cic(P, grid_mass, &info, &conf);
 
-	int index, j, k;
+	int index, i, j, k;
 	for (i = -2; i <= 1; i++) {
 		for (j = -2; j <= 1; j++) {
 			for (k = -2; k <= 1; k++) {
 				index = three_to_one(4 + i, 4 + j, 4 + k, &conf);
 				if (i == -2 || j == -2 || k == -2 || i == 1 || j == 1 || k == 1)
-					assert_that(grid_mass[index],is_equal_to(0));
+					assert_that(grid_mass[index], is_equal_to(0));
 				else
-					assert_that(grid_mass[index],is_equal_to(1.25));
+					assert_that(grid_mass[index],
+								is_equal_to(1 * info.num_of_parts / 8));
 			}
 		}
 	}
 }
 
 Ensure(cic, calculate_the_share_of_grid_right_at_borders) {
-	config_struct conf;
-	conf.num_of_grids_in_each_axis = 10;
-	int tot_num_of_grids = pow(conf.num_of_grids_in_each_axis, 3);
-
-	input_info_struct info;
-	info.num_of_parts = 10;
-	info.box_length = 10;
-
-	particle_data_struct *P;
-	allocate_particle_data_struct(&P, info.num_of_parts);
-
-	int i;
-	for (i = 0; i < info.num_of_parts; i++) {
-		P[i].Pos[0] = 4;
-		P[i].Pos[1] = 0;
-		P[i].Pos[2] = 4;
-		P[i].Mass = 1;
-	}
-
-	double *grid_mass;
-	allocate_double_array(&grid_mass, tot_num_of_grids);
+	fill_P(4, 0, 4, 1);
 
 	cic(P, grid_mass, &info, &conf);
 
@@ -121,6 +86,17 @@ TestSuite *cic_tests() {
 	TestSuite *suite = create_test_suite();
 	add_test_with_context(suite, cic, returns_right_value_for_a_trivial_case);
 	add_test_with_context(suite, cic, calculate_the_share_of_each_cell_right);
-	add_test_with_context(suite, cic, calculate_the_share_of_grid_right_at_borders);
+	add_test_with_context(suite, cic,
+						 calculate_the_share_of_grid_right_at_borders);
 	return suite;
+}
+
+void fill_P (double x, double y, double z, double mass) {
+	int i;
+	for (i = 0; i < info.num_of_parts; i++) {
+		P[i].Pos[0] = x;
+		P[i].Pos[1] = y;
+		P[i].Pos[2] = z;
+		P[i].Mass = mass;
+	}
 }
--- 2_griding/src/include/density_contrast_test.c
+++ 2_griding/src/include/density_contrast_test.c
@@ -6,29 +6,42 @@
 #include "./density_contrast.h"
 
 Describe(density_contrast);
-BeforeEach(density_contrast) {}
+
+#define NUM_OF_GRIDS 2
+#define TOT_NUM_OF_GRIDS 8
+#define BOX_LENGTH 1
+#define MASS 2.0
+#define RANDOM -1.234567
+
+static config_struct conf;
+static input_info_struct info;
+static double mass[TOT_NUM_OF_GRIDS];
+static double delta[TOT_NUM_OF_GRIDS];
+
+BeforeEach(density_contrast) {
+	conf.num_of_grids_in_each_axis = NUM_OF_GRIDS;
+	info.box_length = BOX_LENGTH;
+
+	int i;
+	for (i = 0; i < TOT_NUM_OF_GRIDS; i++) {
+		mass[i] = MASS;
+		delta[i] = RANDOM;
+	}
+}
+
 AfterEach(density_contrast) {}
 
 Ensure(density_contrast, returns_right_value_for_a_trivial_case) {
-	input_info_struct info;
-	info.box_length = 1;
-
-	config_struct conf;
-	conf.num_of_grids_in_each_axis = 2;
-
-	double mass[8] = {2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0, 2.0};
-	double delta[8] = {-1.23, -1.23, -1.23, -1.23, -1.23, -1.23, -1.23, -1.23};
-
 	density_contrast(mass, &info, &conf, delta);
 
 	int i;
-	for (i = 0; i < 8; i++) {
+	for (i = 0; i < TOT_NUM_OF_GRIDS; i++)
 		assert_that(delta[i], is_equal_to(0));
-	}
 }
 
 TestSuite *density_contrast_tests() {
 	TestSuite *suite = create_test_suite();
-	add_test_with_context(suite, density_contrast, returns_right_value_for_a_trivial_case);
+	add_test_with_context(suite, density_contrast,
+						  returns_right_value_for_a_trivial_case);
 	return suite;
 }
