af45df69473b7423e5a846de4058a3d988432948
Saeed Sarpas <saeed@astro.uni-bonn.de>
2016-02-17 20:16:42 +0100

    Included get_config_test to the global_function tests

--- global_functions/tests.c
+++ global_functions/tests.c
@@ -2,12 +2,14 @@
 
 TestSuite *get_mode_in_range_tests();
 TestSuite *three_to_one_tests();
+TestSuite *get_config_tests();
 
 int main(int argc, char **argv) {
 	TestSuite *suite = create_test_suite();
 
 	add_suite(suite, get_mode_in_range_tests());
 	add_suite(suite, three_to_one_tests());
+	add_suite(suite, get_config_tests());
 
 	if (argc > 1) {
 		return run_single_test(suite, argv[1], create_text_reporter());
