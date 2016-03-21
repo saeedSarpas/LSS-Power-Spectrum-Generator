e032295a9fa81b71478b65a26138ef8c2ed56998
saeedSarpas <s.sarpas@gmail.com>
2016-03-21 18:16:14 +0100

    Modified signal_power function based on new find_first_mode_with_modulus function

--- 5a_power_spectrum/src/include/signal_power.c
+++ 5a_power_spectrum/src/include/signal_power.c
@@ -1,3 +1,23 @@
+/**
+ * signal_power.c
+ * @test_file signal_power_test.c
+ *
+ * Calculating the power of a signal (or more accurately all the signals within
+ * k_min <= mode_modulus < k_max).
+ *
+ * @param k_min Minimum of the mode modulus
+ * @param k_max Maximum of the mode modulus
+ * @param delta_fourier Fourier density contrast array
+ * @param sorted_modes_array Sorted array of modes
+ * @param conf Configurations
+ *
+ * @return signal_power_result_struct which contains the power of the signal and
+ * its error (the error is calculated using the standard deviation of the
+ * powers)
+ *
+ * @author Saeed Sarpas
+ */
+
 #include <stdlib.h>
 #include <complex.h>
 #include <fftw3.h>
@@ -9,7 +29,7 @@
 
 #include "./../../../global_functions/vector/vector.h"
 
-#include "./../../../global_functions/modes/get_modes_in_range.h"
+#include "./../../../global_functions/modes/find_first_mode_with_modulus.h"
 #include "./../../../global_functions/grid/three_to_one.h"
 
 #include "./../struct/signal_power_result.h"
@@ -17,79 +37,77 @@
 #include "./../include/generate_logarithmic_bins.h"
 
 typedef struct {
-	double mode_power;
-	double error;
+    double mode_power;
+    double error;
 } variance_result_struct;
 
 static variance_result_struct variance(vector_struct *powers_vector) {
 
-	variance_result_struct result;
-	result.mode_power = 0.0;
-	result.error = 0.0;
+    variance_result_struct result;
+    result.mode_power = 0.0;
+    result.error = 0.0;
 
-	unsigned int i;
-	double power;
-	for (i = 0; i < powers_vector->log_length; i++) {
-		vector_get_elem(powers_vector, i, &power);
-		result.mode_power += power;
-	}
+    unsigned int i;
+    double power;
+    for (i = 0; i < powers_vector->log_length; i++) {
+        vector_get_elem(powers_vector, i, &power);
+        result.mode_power += power;
+    }
 
-	result.mode_power /= powers_vector->log_length;
+    result.mode_power /= powers_vector->log_length;
 
-	int tot_num_of_powers = powers_vector->log_length;
+    int tot_num_of_powers = powers_vector->log_length;
 
-	double error = 0.0;
-	while (powers_vector->log_length > 0) {
-		vector_pop(powers_vector, &power);
-		error += pow(power - result.mode_power, 2);
-	}
+    double error = 0.0;
+    while (powers_vector->log_length > 0) {
+        vector_pop(powers_vector, &power);
+        error += pow(power - result.mode_power, 2);
+    }
 
-	result.error = sqrt(error) / tot_num_of_powers;
+    result.error = sqrt(error) / tot_num_of_powers;
 
-	return result;
+    return result;
 }
 
 signal_power_result_struct signal_power (double k_min, double k_max,
-		fftw_complex *delta_fourier, modes_struct *indexed_mode_modulus,
-		config_struct *conf) {
+                                         fftw_complex *delta_fourier,
+                                         modes_struct *sorted_modes_array,
+                                         config_struct *conf) {
 
-	signal_power_result_struct result;
+    signal_power_result_struct result;
 
-	size_t tot_num_of_grids = pow(conf->params.numOfAxisGrids, 3);
+    size_t tot_num_of_grids = pow(conf->params.numOfAxisGrids, 3);
 
-	vector_struct modes_vector;
-	vector_new(&modes_vector, sizeof(struct modes), tot_num_of_grids);
+    int min_index = find_first_mode_with_modulus(
+        k_min, sorted_modes_array, tot_num_of_grids);
+    int max_index = find_first_mode_with_modulus(
+        k_max, sorted_modes_array, tot_num_of_grids);
 
-	get_modes_in_range(k_min, k_max, indexed_mode_modulus, conf, &modes_vector);
-	result.num_of_found_modes = modes_vector.log_length;
+    vector_struct powers_vector;
+    vector_new(&powers_vector, sizeof(double), max_index - min_index + 1);
 
-	vector_struct powers_vector;
-	vector_new(&powers_vector, sizeof(double), modes_vector.log_length);
+    double power;
+    int i;
+    for (i = min_index; i < max_index; i++) {
+        power = pow(creal(delta_fourier[sorted_modes_array[i].index]), 2) +
+            pow(cimag(delta_fourier[sorted_modes_array[i].index]), 2);
 
-	modes_struct mode;
-	double power;
-	while (modes_vector.log_length > 0) {
-		vector_pop(&modes_vector, &mode);
+        vector_push(&powers_vector, &power);
+    }
 
-		power = pow(creal(delta_fourier[mode.index]), 2) +
-			pow(cimag(delta_fourier[mode.index]), 2);
+    result.num_of_found_modes = max_index - min_index - 1;
 
-		vector_push(&powers_vector, &power);
-	}
+    if (result.num_of_found_modes == 0) {
+        /* There is a memory leak here! Exiting before disposing powers vector*/
+        printf("\n[Found no point in this shell: %f <= k < %f]\n", k_min, k_max);
+        exit(EXIT_FAILURE);
+    } else {
+        variance_result_struct variance_result = variance(&powers_vector);
+        result.mode_power = variance_result.mode_power;
+        result.error = variance_result.error;
+    }
 
-	if (result.num_of_found_modes == 0) {
-		printf("\n[Found no point in this shell: %f <= k < %f]\n", k_min, k_max);
-		vector_dispose(&modes_vector);
-		vector_dispose(&powers_vector);
-		exit(0);
-	} else {
-		variance_result_struct variance_result = variance(&powers_vector);
-		result.mode_power = variance_result.mode_power;
-		result.error = variance_result.error;
-	}
+    vector_dispose(&powers_vector);
 
-	vector_dispose(&modes_vector);
-	vector_dispose(&powers_vector);
-
-	return result;
+    return result;
 }
--- 5a_power_spectrum/src/include/signal_power_test.c
+++ 5a_power_spectrum/src/include/signal_power_test.c
@@ -1,3 +1,10 @@
+/**
+ * signal_power_test.c
+ * @test_for signal_power.c
+ *
+ * @author Saeed Sarpas
+ */
+
 #include <cgreen/cgreen.h>
 #include <math.h>
 #include <complex.h>
@@ -7,8 +14,7 @@
 
 #include "./../../../global_functions/memory/allocate.h"
 #include "./../../../global_functions/grid/three_to_one.h"
-
-#include "./../../../4_indexing_k_modulus/src/include/sort.h"
+#include "./../../../global_functions/modes/sort_modes.h"
 
 #include "./../../src/struct/signal_power_result.h"
 
@@ -29,64 +35,61 @@ static void fill_delta_fourier();
 static void fill_indexed_mode_modulus();
 
 BeforeEach(signal_power) {
-	conf.params.numOfAxisGrids = NUM_OF_GRIDS;
-
-	tot_num_of_grids = pow(conf.params.numOfAxisGrids, 3);
-
-	allocate((void **)&delta_fourier, tot_num_of_grids, sizeof(fftw_complex));
-
-	allocate((void **)&indexed_mode_modulus, pow(NUM_OF_GRIDS, 3),
-			 sizeof(modes_struct));
+    conf.params.numOfAxisGrids = NUM_OF_GRIDS;
+    tot_num_of_grids = pow(conf.params.numOfAxisGrids, 3);
+    allocate((void **)&delta_fourier, tot_num_of_grids, sizeof(fftw_complex));
+    allocate((void **)&indexed_mode_modulus, pow(NUM_OF_GRIDS, 3),
+             sizeof(modes_struct));
 }
 
 AfterEach(signal_power) {
-	free(delta_fourier);
-	free(indexed_mode_modulus);
+    free(delta_fourier);
+    free(indexed_mode_modulus);
 }
 
 Ensure(signal_power, returns_right_results_for_a_trivial_input) {
-	fill_delta_fourier();
-	fill_indexed_mode_modulus();
+    fill_delta_fourier();
+    fill_indexed_mode_modulus();
 
-	signal_power_result_struct result;
-	result = signal_power(0, 10000, delta_fourier,
-			indexed_mode_modulus, &conf);
+    signal_power_result_struct result;
+    result = signal_power(0, 10000, delta_fourier,
+                          indexed_mode_modulus, &conf);
 
-	double power = pow(DELTA_FOURIER_REAL_PART, 2) +
-		pow(DELTA_FOURIER_IMAG_PART, 2);
+    double power = pow(DELTA_FOURIER_REAL_PART, 2) +
+        pow(DELTA_FOURIER_IMAG_PART, 2);
 
-	assert_that(result.mode_power, is_equal_to(power));
-	assert_that(result.error, is_equal_to(0));
-	assert_that(result.num_of_found_modes, is_equal_to(10000));
+    assert_that(result.mode_power, is_equal_to(power));
+    assert_that(result.error, is_equal_to(0));
+    assert_that(result.num_of_found_modes, is_equal_to(9999));
 }
 
 TestSuite *signal_power_tests() {
-	TestSuite *suite = create_test_suite();
-	add_test_with_context(suite, signal_power,
-						 returns_right_results_for_a_trivial_input);
-	return suite;
+    TestSuite *suite = create_test_suite();
+    add_test_with_context(suite, signal_power,
+                          returns_right_results_for_a_trivial_input);
+    return suite;
 }
 
 void fill_delta_fourier() {
-	int i;
-	for(i = 0; i < tot_num_of_grids; i++)
-		delta_fourier[i] = DELTA_FOURIER_REAL_PART + DELTA_FOURIER_IMAG_PART * I;
+    int i;
+    for(i = 0; i < tot_num_of_grids; i++)
+        delta_fourier[i] = DELTA_FOURIER_REAL_PART + DELTA_FOURIER_IMAG_PART * I;
 }
 
 void fill_indexed_mode_modulus() {
-	int index, i, j, k;
-	for (i = 0; i < NUM_OF_GRIDS; i++) {
-		for (j = 0; j < NUM_OF_GRIDS; j++) {
-			for (k = 0; k < NUM_OF_GRIDS; k++) {
-				index = three_to_one(i, j, k, &conf);
-				indexed_mode_modulus[index].index = index;
-				indexed_mode_modulus[index].kx = i - (NUM_OF_GRIDS / 2);
-				indexed_mode_modulus[index].ky = j - (NUM_OF_GRIDS / 2);
-				indexed_mode_modulus[index].kz = k - (NUM_OF_GRIDS / 2);
-				indexed_mode_modulus[index].modulus = index;
-			}
-		}
-	}
+    int index, i, j, k;
+    for (i = 0; i < NUM_OF_GRIDS; i++) {
+        for (j = 0; j < NUM_OF_GRIDS; j++) {
+            for (k = 0; k < NUM_OF_GRIDS; k++) {
+                index = three_to_one(i, j, k, &conf);
+                indexed_mode_modulus[index].index = index;
+                indexed_mode_modulus[index].kx = i - (NUM_OF_GRIDS / 2);
+                indexed_mode_modulus[index].ky = j - (NUM_OF_GRIDS / 2);
+                indexed_mode_modulus[index].kz = k - (NUM_OF_GRIDS / 2);
+                indexed_mode_modulus[index].modulus = index;
+            }
+        }
+    }
 
-	sort(indexed_mode_modulus, &conf);
+    sort_modes(indexed_mode_modulus, &conf);
 }
